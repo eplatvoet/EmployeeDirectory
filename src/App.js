ef5221dd15bea84eff6ef873ee6dd4e6e179da4b
Erin Platvoet <erin.didomenico520@gmail.com>
2020-09-20 23:23:18 -0400

    deleted files, copied over files from new create-react-app, built staff.json information, added logo to public index

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component }from 'react';
+import './App.css';
+
+function App () {
+  return (
+    <div>
+        <center><h1>Scranton Branch:</h1></center>
+        {/* components go in */}
+
+    </div>
+  );
+}
+
+export default App;
